4499128b0190ebe92aeb3cd0b24f66f2c035e5aa
Ihor <igortrofymov@gmail.com>
2024-02-24 13:11:48 +0200

    end of section 7

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,44 @@
+import { observer } from 'mobx-react-lite';
+import { SyntheticEvent, useState } from 'react';
+import {Button, Item, Label, Segment} from "semantic-ui-react";
+import { useStore } from '../../../app/stores/store';
+
+export default observer(function ActivityList() {
+    const {activityStore} = useStore();
+    const {deleteActivity, activitiesByDate, loading} = activityStore;
+
+    const [target, setTarget] = useState('');
+
+    function handleDeleteActivity(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        setTarget(e.currentTarget.name);
+        deleteActivity(id)
+    }
+    
+    return (
+        <Segment>
+            <Item.Group divided>
+                {activitiesByDate.map(activity => (
+                    <Item key={activity.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Meta>{activity.date}</Item.Meta>
+                            <Item.Description>
+                                <div>{activity.description}</div>
+                                <div>{activity.city}, {activity.venue}</div>
+                            </Item.Description>
+                            <Item.Extra>
+                                <Button floated='right' content='View' color='blue'
+                                        onClick={() => activityStore.selectActivity(activity.id)}/>
+                                <Button loading={loading && target === activity.id}
+                                        name={activity.id} floated='right' content='Delete'
+                                        color='red'
+                                        onClick={(e) => handleDeleteActivity(e, activity.id)}/>
+                                <Label basic content={activity.category}/>
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </Segment>
+    )
+})
